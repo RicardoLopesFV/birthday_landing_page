27bb351849fdc06693f0c5ef204f78ef00803e5c
Ricardo Lopes <ricardolopessfv@hotmail.com>
2023-07-08 00:15:10 -0300

    End of project

---
+++ src/scripts/main.js
@@ -0,0 +1,27 @@
+
+const dataDoEvento = new Date("Dec 12, 2023 22:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(() => {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    const diasEmMs = 1000 * 60 * 60 * 24;
+    const horasEmMs = 1000 * 60 * 60;
+    const minutosEmMs = 1000 * 60;
+    const segundosEmMs = 1000;
+
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diasEmMs);
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diasEmMs) / horasEmMs);
+    const minutosAteOEvento = Math.floor((distanciaAteOEvento % horasEmMs) / minutosEmMs);
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutosEmMs) / segundosEmMs);
+    
+    document.getElementById('contador').innerHTML = `${diasAteOEvento} D . ${horasAteOEvento} H . ${minutosAteOEvento} M . ${segundosAteOEvento} S`;
+
+    if (distanciaAteOEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML = 'Evento Expirado!';
+    }
+}, 1000);
